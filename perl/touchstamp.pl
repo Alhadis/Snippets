533505f8cebf95ad0c26ebec49f705b8b11bd238
Alhadis <gardnerjohng@gmail.com>
2015-04-29 22:23:18 +1000

    Add tool for changing/reading file timestamps
    I find the "[[CC]YY]MMDDhhmm[.SS]" syntax of Unix's touch tool to be too
    cumbersome, preferring to pass a Unix-style timestamp intead. Similarly,
    I don't get much joy from using `stat -f %m $FILE` each time I'd like to
    parse a file's modification time in a POSIX-friendly fashion.

---
+++ perl/touchstamp.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+# Sets the access and modification times of a file using a Unix timestamp.
+#
+# This could probably have been written using a simple shell-script; however,
+# I've kinda cracked the shits with Bash's handling of timestamps and
+# formatting.
+
+use warnings;
+use strict;
+use feature "say";
+
+
+# Queried filename
+my $filename	=	shift(@ARGV);
+
+
+# If a timestamp was provided, use it to set $filename's modification/access times.
+if(my $timestamp = shift(@ARGV)){
+	utime $timestamp, $timestamp, $filename;
+}
+
+# Otherwise, just return the file's current modification time.
+else{
+	my @stats	=	stat($filename);
+	say $stats[9];
+}
