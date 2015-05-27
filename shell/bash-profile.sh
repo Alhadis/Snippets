c7766d77d41155beebf6368f385216c737131878
Alhadis <gardnerjohng@gmail.com>
2015-05-27 20:49:51 +1000

    Update .bash_profile additions
      - Aliased a helpful AppleScript one-liner to full-screen Chrome's
        window when it's been resized (since Mac OS's maximise button is
        as useless as a screen-door on a submarine).
    
      - Aliased a 10-paragraph generation of "Lorem Ipsum" text.
    
      - Set the default directory for new Terminal windows to the Desktop.

--- shell/bash-profile.sh
+++ shell/bash-profile.sh
@@ -6,6 +6,8 @@ alias g='git status'
 alias u='pbpaste | uglifyjs --mangle 2>/dev/null | pbcopy'
 alias strip-meta='exiftool $@ "-All=" -overwrite_original'
 alias fuck-this-shit='git reset --hard HEAD; git clean -fd'
+alias lipsum='lorem-ipsum 10 paragraphs'
+alias fit-chrome='osascript -e '"'"'tell first window of application "Google Chrome" to set bounds to {0, 0, 1440, 820}'"'"
 
 
 # Global function for full-screening the terminal window.
@@ -24,3 +26,4 @@ export fit; fit
 
 # Various other crap
 { rm ~/.DS_Store; dsclean ~/Desktop; } > /dev/null 2>&1
+cd ~/Desktop;
