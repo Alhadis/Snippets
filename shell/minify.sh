8297896e29d1394be96129746763be7fd4317bad
Alhadis <gardnerjohng@gmail.com>
2015-05-27 11:15:15 +1000

    Change filenames of minified resources
    "Main" is a misleading choice for a minified file's name when one of the
    source files it's generated from is also named "main". For example, it's
    possible for a reader to misconstrue "min/main.css" as a compressed form
    of "css/main.css" alone, not taking into account the preceding two files
    prepended to it ("css/fonts.css" & "css/global.css").

--- shell/minify.sh
+++ shell/minify.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Stylesheets
-cat css/fonts.css css/global.css css/main.css | cleancss --skip-advanced -o min/main.css
+cat css/fonts.css css/global.css css/main.css | cleancss --skip-advanced -o min/min.css
 
 
 # JavaScript
-uglifyjs js/main.js --mangle -o min/main.js
+uglifyjs js/main.js --mangle -o min/min.js
