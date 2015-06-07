64d824a310da6734f7d23e364308c5ebfa08f66e
Alhadis <gardnerjohng@gmail.com>
2015-06-07 18:52:14 +1000

    Add one-liner to generate an HTML options list

--- shell/bash-profile.sh
+++ shell/bash-profile.sh
@@ -8,6 +8,7 @@ alias strip-meta='exiftool $@ "-All=" -overwrite_original'
 alias fuck-this-shit='git reset --hard HEAD; git clean -fd'
 alias lipsum='lorem-ipsum 10 paragraphs'
 alias fit-chrome='osascript -e '"'"'tell first window of application "Google Chrome" to set bounds to {0, 0, 1440, 820}'"'"
+alias html-day-options='output=""; for i in {1..31}; do output+=$(printf "<option value=\"%d\">%02d</option>" $i $i); done; echo $output | pbcopy';
 alias apl='apl --noColor'
 alias md5='md5 -q'
 
