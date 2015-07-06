1511e198b28d9f55d2ea7bb3bc3a2eec9ce3d555
Alhadis <gardnerjohng@gmail.com>
2015-07-06 21:59:16 +1000

    Add shell-script for updating a folder of Git repos
    This script simply cycles through a directory of repositories, calling
    git's "pull" subcommand in each one, printing feedback to STDOUT as it
    goes. Just a simple way of keeping numerous local forks up-to-date with
    their remote counterparts.

---
+++ shell/update-repos.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Neuter the internal field separator
+IFS=''
+
+# Formatting variables
+BOLD=`tput bold`
+BLUE=`tput setaf 12`
+RESET=`tput sgr0`
+
+
+# Cycle through each item in the CWD
+for i in *; do
+	
+	# Ensure the item's really a Git repository
+	[ -d "$i/.git" ] && { echo $(
+
+		cd $i;
+
+		# Print a divider to STDOUT to break the feedback up better
+		printf %s$'\n' "${BOLD}${BLUE}==>${RESET}${BOLD} Updating: $i${RESET}";
+
+		# Update!
+		git status --porcelain && { git pull; };
+
+	); };
+done;
