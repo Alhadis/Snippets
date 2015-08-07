e4e99e56b5cbc54438676e96d143415f64420acd
Alhadis <gardnerjohng@gmail.com>
2015-08-07 23:14:04 +1000

    Extend Rainbow program with new options
    The xterm-colour viewer now accepts the following options:
    
      -t  Declares a custom string to use instead of the alphabet
    
      -b  Fills each row with a solid-coloured bar using codepoint U+2588.
          Overrides the value of the -t option, if passed previously.
    
    The program also checks the terminal's capabilities before attempting to
    print anything. An error message will be printed to STDERR if running in
    an environment that lacks support for coloured output.
    
    The terminal's reported colour range will be used to limit values passed
    to the -c option. For example, it's now impossible to request a range of
    colours greater than 256 when running on an xterm-256color terminal.

--- shell/rainbow.sh
+++ shell/rainbow.sh
@@ -1,32 +1,61 @@
 #!/bin/sh
 
 IFS=
-alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ - 1234567890 - abcdefghijklmnopqrstuvwxyz";
+text="ABCDEFGHIJKLMNOPQRSTUVWXYZ - 1234567890 - abcdefghijklmnopqrstuvwxyz";
 reset=$(tput sgr0);
-max=256
+max=$(tput colors);
+count=$max;
+
+
+# Bail if coloured output's not supported
+[ "$max" -lt 1 ] && {
+	>&2 echo "ERROR: Terminal doesn't support coloured output. Aborting.";
+	exit 1;
+};
+
+
+# Print a solid-colour bar to STDOUT
+bar () {
+	length=${1:-80}
+	char=$(echo "\xe2\x96\x88")
+	echo $(printf "%${length}s" '' | tr ' ' $char)
+}
+
+# Prints a usage message and exits
+usage () {
+	name=$(basename "$0");
+	echo "usage: $name [-alb] [c <count>] [t <text>]";
+}
+
+
 
 
 # Show only the first 16 colours by default
 count=15
 
 # Parse any options we were given
-while getopts alc: option; do
+while getopts alc:t:b option; do
 	case $option in
 	a)	count=$max				;;	# Show all colours
 	l)	count=$max				;;	# Synoynm for -a: displays a long rainbow
-	c)	count=$((OPTARG - 1))	;;	# Displays an arbitrary number of colours
+	c)	count=$((OPTARG - 1))	;;	# Display an arbitrary number of colours
+	t)	text=$OPTARG			;;	# Use a custom string to display each row
+	b)	text=$(bar 73)			;;	# Use a solid-coloured bar instead of text
+	?)	>&2 usage; exit 1;		;;	# Unknown option: print usage message and exit
 	esac
 done;
 
 
-[ $count -le 0 ] && { count=0; }
+# Cap the colour-count to the terminal's maximum number of possible colours
+[ $count -gt $max ] && { count=$max; }
+
 
 for((i = 0; i <= $count; i++)); do
 	colour=$(tput setaf $i);
 	space=" ";
 	[ $i -lt 10 ] && { space="  "; }
 	[ $i -gt 99 ] && { space=""; }
-	echo "${reset}${space}$i   ${colour}${alphabet}";
+	echo "${reset}${space}$i   ${colour}${text}";
 done;
 
 echo $reset;
