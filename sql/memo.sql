e78e17c8c7762ae06189a1388d4ac528a2688daa
Alhadis <gardnerjohng@gmail.com>
2015-08-11 08:52:03 +1000

    Add folder for SQL snippets

---
+++ sql/memo.sql
@@ -0,0 +1,15 @@
+# Querying rows whose IDs are included in a predetermined set
+SELECT users_id, max(date) from `TABLE` WHERE users_id in (1,2,3,4,5) GROUP BY 1
+
+# Results from last week
+SELECT * FROM entries WHERE submitted BETWEEN date_sub(now(), INTERVAL 1 WEEK) and now();
+
+# INNER JOIN example
+SELECT
+	users.*,
+	groups.*
+
+	FROM			users
+	INNER JOIN		groups
+		WHERE		groups.id = 1
+		ORDER BY	users.id;
