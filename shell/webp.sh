defffd55a4654c601b211278bc9e6af5b72d3e3d
Alhadis <gardnerjohng@gmail.com>
2015-08-29 00:39:17 +1000

    Fix broken path-handling in WebP wrapper
    This commit enables files to be targeted in different directories.

--- shell/webp.sh
+++ shell/webp.sh
@@ -4,9 +4,10 @@
 [[ ! $1 ]] && {>&2 echo "ERROR: No argument passed."; exit 1; } || {
 
 	[ -f $1 ] && {
-		dirname=$(dirname "$1" | xargs realpath);
-		filename=$(basename "$1");
-		cwebp "$filename" -o "$dirname/"$(echo "$filename" | sed -E "s/(\.[A-Za-z0-9]+)$//")".webp";
+		abspath=$(realpath "$1")
+		dirname=$(dirname "$abspath");
+		filename=$(basename "$abspath");
+		cwebp "$abspath" -o "$dirname/"$(echo "$filename" | sed -E "s/(\.[A-Za-z0-9]+)$//")".webp";
 	} || {
 		>&2 echo "ERROR: \"$1\" isn't a valid image file.";
 		exit 1;
