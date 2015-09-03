431e36f291f6f5e482b6f38d45c4a937ee07f9dc
Alhadis <gardnerjohng@gmail.com>
2015-09-03 20:56:19 +1000

    Add script to clean-up IcoMoon-generated CSS

---
+++ perl/clean-icomoon.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use open qw/:std :utf8/;
+
+
+# Slurp mode
+$/=undef;
+
+while(<>){
+	s/'/"/g;
+	s/\.([-\w]+):before\s*\{\s+content:\s*("\\\w+");\s*\}/\.$1::before\t\t\t\{\tcontent: $2;\t\}/gmi;
+	s/"\\([0-9A-Fa-f]{2})"/'"'.chr(hex($1)).'"'/gmei;
+	s/ (?=\{\n)|^\.icon-rot::before\s*\{[^\}]+\}//gm;
+	s/\.icon-/.i./g;
+	s/(?<=[\t:])(?=url)/ /g;
+	s/\.eot\?#iefix([^'"]+)/\.eot#iefix/g;
+	s/\?${1}//g;
+	s/\[class\^="icon-"\][^{]+\{[^\}]+\}\n{2}//gmi;
+	print STDOUT;
+}
