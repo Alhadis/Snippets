916f39d9eb366e88271b29bae5be5b3f7ef48859
Alhadis <gardnerjohng@gmail.com>
2015-09-24 21:11:20 +1000

    Extend form autofill script
    When run, the script will now:
    
    * Look for inputs with matching "name" attributes if no IDs are matched
    * Check for snake_cased and camelCased variants of each field name
    * Check if <select> elements weren't assigned anything; if not, a random
      option from the field is selected instead.
    
    Also contains a few extra field names to populate when executed.

--- js/autofill.js
+++ js/autofill.js
@@ -1,29 +1,40 @@
 function autoFill(){
-	var auto	=	{
+	var auto	= {
 		"first-name":		"John",
 		"last-name":		"Gardner",
+		"surname":			"Gardner",
 		"dob-day":			18,
 		"dob-month":		"03",
 		"dob-year":			1987,
+		"dob":				"1987-03-18",
 		"city":				"Melbourne",
 		"locality":			"Melbourne",
 		"postcode":			3717,
 		"region":			"VIC",
-		"street-addr":		"65 Lotus Crescent",
+		"business-name":	"Offline Square Analogue",
+		"street-address":	"65 Lotus Crescent",
 		"phone-number":		"(02) 6056 5455",
-		"email-addr":		"test@domain.com",
+		"email-address":	"test@domain.com",
 		"answer":			"Lorem ipsum dolor sit amet",
 		"caption":			"Eiusmod quis ipsum proident dolor",
+		"invoice-number":	781676020027,
 		"accepted-terms":	true
-	}, i, field;
+	}, i, j, field;
 
 	for(i in auto){
-		
-		if(field = document.getElementById(i)){
-			if("checkbox" === field.type)
-					field.checked	=	!!auto[i];
-			else	field.value		=	auto[i];
-		}
+
+		[i, i.replace(/-/g, "_"), i.replace(/-([a-z])/g, function(s,i){ return i.toUpperCase() })].forEach(function(j){
+			if((field = document.getElementById(j)) || (field = document.querySelector("input[name='"+j+"']"))){
+				if("checkbox" === field.type)
+					 field.checked	= !!auto[i];
+				else field.value	= auto[i];
+
+				if("SELECT" === field.tagName && !field.value){
+					var optval	=	Array.prototype.map.call(field.options, function(s){ return s.value || null }).filter(function(i){ return !!i });
+					field.value	=	optval[ Math.round(Math.random() * (optval.length - 1)) ];
+				}
+			}
+		});
 	}
 }
 autoFill();
