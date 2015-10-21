dde8c27061af1e926b9a81561e9ff5cd9ca497e6
Alhadis <gardnerjohng@gmail.com>
2015-10-22 04:11:45 +1100

    Improve Object.defineProperty patch for IE8
    To enable getters/setters on native JavaScript objects in IE8, apply the
    IE8PP function to them before calling Object.defineProperty:
    
        var Singleton   = {};
        var currentPage = 1;
    
        /** Get this working in IE8 */
        Singleton       = IE8PP( Singleton );
        Object.defineProperty(Singleton, "page", {
            get: function(){ return currentPage },
            set: function(i){ currentPage = i; }
        });
    
    Comment out the call to IE8PP, and the browser will choke the second you
    target it with Object.defineProperty. Amazing, huh?

--- js/IE8-defineProperty.js
+++ js/IE8-defineProperty.js
@@ -1,74 +1,34 @@
 /**
- * For JavaScript "classes" that use Object.defineProperty AND operate on a DOM element, this hacky method
- * applies a workaround for IE8's inability to define properties on native JavaScript objects. Rather than
- * generating an instance of the class, the code forces the constructor to point to the relevant DOM element,
- * allowing getter/setter pairs defined by the class to operate transparently. Not a glamorous workaround, but
- * it works.
+ * IE8 Property Patch
  *
- * If you're getting inexplicable runtime errors in IE8 ("Not implemented"), be sure your code's not overwriting
- * a property defined by the subject element's DOM interface (examples include "dir", "lang", "id", etc).
+ * This stupid hack attempts to circumvent IE8's inability to define properties
+ * on native JS objects by merging their properties/methods into a detached DOM
+ * element and returning it.
  *
- * @param {Function} fn - Pointer to the JavaScript "class"
- * @param {Number} argIndex - Index of the constructor argument that holds the class's subject DOM reference.
- * @param {String} argName - If set, will use the property of the argument located above as the subject element.
- * @return {Function}
+ * @param {Function|Object} obj - Native JavaScript object before it's targeted
+ *                                by Object.defineProperty.
+ * @return {HTMLElement}
  */
-function IE8PropertyPunch(fn, argIndex, argName){
-	return function(){
-		var p, args	=	arguments,
-			THIS	=	args[argIndex || 0];
-		if(argName)
-			THIS	=	THIS[argName];
+var IE8PP = function(obj){
 
-		/** TODO: Add support for prototype's superclasses? */
-		for(p in fn.prototype)
-			THIS[p]	=	fn.prototype[p];
+	if(obj instanceof Element)
+		return obj;
 
-		fn.apply(THIS, args);
-		return THIS;
-	};
-}
+	if("function" === typeof obj)
+		return function(){
+			var shadow = document.createElement("s");
+			for(var p in obj.prototype)
+				shadow[p] = obj.prototype[p];
+			shadow.prototype = obj.prototype;
+			obj.apply(shadow, arguments);
+			return shadow;
+		}
 
-
-
-
-
-
-
-/**
- * Example usage
- *
- * I understand this code has an extremely specific use that's not easily explained...
- * so an example might enlighten baffled readers a bit better. The following code illustrates
- * a common-use scenario where a developer's using a JavaScript "class" to implement AJAX-powered
- * forms, with the first argument passed to an AJAXForm instance being an existing <form> element.
- *
- * Since IE8 can't define getter/setter properties on native JS objects, the entire underlying functionality
- * of the class is ruined - unless we point the constructor to operate within the context of the DOM element
- * it operates on. Suddenly, IE8 has no issues with defineProperty! Huzzah! Effective? Yes! Beautiful? NO!
- */
-var AJAXForm	=	function(args){
-	for(var i in args)
-		this[i]	=	args[i];
-
-	var loading	=	false;
-	Object.defineProperty(this, "loading", {
-		get:	function(){ return loading; },
-		set:	function(input){ loading	=	input; }
-	});
+	else{
+		var shadow = document.createElement("s");
+		for(var p in obj)
+			shadow[p] = obj[p];
+		shadow.prototype = obj;
+		return shadow;
+	}
 };
-
-/** Check if the browser can't use Object.defineProperty on native JavaScript objects. */
-if(function(){
-	try{Object.defineProperty({}, "test", {value: true});}
-	catch(e){return true;}
-}()){
-
-	/** If not, hack this shit. */
-	AJAXForm	=	IE8PropertyPunch(AJAXForm, 0, "form");
-}
-
-
-var f	=	new AJAXForm({
-	form: 	document.getElementById("entry-form")
-});
