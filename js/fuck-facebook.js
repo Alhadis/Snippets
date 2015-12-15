0e78784376abc07b16f277a14cc3db9ffffb9a84
Alhadis <gardnerjohng@gmail.com>
2015-12-16 00:49:42 +1100

    Add bookmarklet to kill FB's registration nag
    Facebook now displays a registration screen to unregistered users asking
    them to sign up while browsing public pages. Since this is a pain in the
    arse for managing band submissions at Metal Archives, I've written a toy
    to kick Facebook in the gonads. So to speak.
    
    Copy the last line of the script to a new browser bookmarklet, and click
    it whenever the soulless, corporate, parasitic behemoth asks you to give
    yourself up to it.
    
    Fuck off.

---
+++ js/fuck-facebook.js
@@ -0,0 +1,10 @@
+javascript:(function(){
+	var bullshit = document.querySelector(".timelineSignUpDialog");
+	bullshit.parentNode.removeChild(bullshit);
+	[].forEach.call(document.querySelectorAll("._31e"), function(e){
+		e.style.cssText += "position: static !important;";
+	});
+}())
+
+/** Compressed/bookmarklet version */
+javascript:(function(){var e=document.querySelector(".timelineSignUpDialog");e.parentNode.removeChild(e);[].forEach.call(document.querySelectorAll("._31e"),function(e){e.style.cssText+="position: static !important;"})})();
