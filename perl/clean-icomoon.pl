2d920cde28cad1ba446a4eec30bd01bf6361c72c
Alhadis <gardnerjohng@gmail.com>
2015-12-24 00:01:49 +1100

    Convert 4-space sequences to real indentation
    IcoMoon, like every other conformist dickhead working in web development
    these days, has decided to use spaces for indenting their CSS output. So
    be it, I'll continue doing this the hard way until a more Unixy solution
    is developed in future (see https://github.com/Alhadis/IconForge).

--- perl/clean-icomoon.pl
+++ perl/clean-icomoon.pl
@@ -9,13 +9,15 @@ $/=undef;
 
 while(<>){
 	s/'/"/g;
+	s/\x20{4}/\t/g;
 	s/\.([-\w]+):before\s*\{\s+content:\s*("\\\w+");\s*\}/\.$1::before\t\t\t\{\tcontent: $2;\t\}/gmi;
 	s/"\\([0-9A-Fa-f]{2})"/'"'.chr(hex($1)).'"'/gmei;
 	s/ (?=\{\n)|^\.icon-rot::before\s*\{[^\}]+\}//gm;
 	s/\.icon-/.i./g;
 	s/(?<=[\t:])(?=url)/ /g;
-	s/\.eot\?#iefix([^'"]+)/\.eot#iefix/g;
+	s/\.eot\?([^'"]+)#iefix/\.eot#iefix/g;
 	s/\?${1}//g;
 	s/\[class\^="icon-"\][^{]+\{[^\}]+\}\n{2}//gmi;
+	s/src:\t /src: /g;
 	print STDOUT;
 }
