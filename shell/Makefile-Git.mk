d8c2ff4304f49d19a7f4d312b11e3e263a0ef2b8
Alhadis <gardnerjohng@gmail.com>
2016-01-01 19:37:54 +1100

    Revert "Add a new makefile for running repo-specific tasks"
    This reverts commit fd8119a83d01d3daac2541ed674397e3bd8700d9.
    
    Ugh, I obviously shouldn't be coding until 6am.

--- shell/Makefile-Git.mk
+++
@@ -1,9 +0,0 @@
-# Make recipes specific for managing git-repos
-
-check_hook := https://raw.githubusercontent.com/Alhadis/Snippets/master/shell/check-version.sh
-
-# Installs some git-hooks to prevent hasty version-committing
-git-hooks:
-	@hash node 2>&- || { >&2 echo "These hooks require Node.js to run."; exit 2; }
-	@curl -s $(check_hook) > .git/hooks/commit-msg
-	@chmod +x .git/hooks/commit-msg
