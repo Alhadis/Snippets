a55f9ebba991ea9a91f4fe8d04248f3e75aff022
Alhadis <gardnerjohng@gmail.com>
2016-01-08 21:22:57 +1100

    Use a less invasive approach to store TransitionEvent support

--- js/polyfills/shim-transitionEnd.js
+++ js/polyfills/shim-transitionEnd.js
@@ -2,14 +2,13 @@
  * Mediate those wretched vendor prefixes for onTransitionEnd.
  *
  * Older browsers only supported the event with prefixed names (Opera being
- * the worst offender). Whichever name is needed to support transition events
- * in a running browser (prefixed or not) is stored in TransitionEvent.END.
+ * the worst offender). The function returns whichever name is needed to
+ * support transition events in a running browser (prefixed or not).
  *
  * URL: https://developer.mozilla.org/en-US/docs/Web/Events/transitionend
  */
-(function(o){
-	if(o.END) return;
+ var transitionEnd = (function(){
 	for(var names = "transitionend webkitTransitionEnd oTransitionEnd otransitionend".split(" "), i = 0; i < 4; ++i)
-		if("on"+names[i].toLowerCase() in window) return (o.END = names[i]);
-	return (o.END = names[0]);
-}(window.TransitionEvent || {}));
+		if("on"+names[i].toLowerCase() in window) return names[i];
+	return names[0];
+}());
