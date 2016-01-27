9550e5ac2f7a2e18f76994f7605983d599cc03e9
Alhadis <gardnerjohng@gmail.com>
2016-01-28 03:54:11 +1100

    Add IE8 polyfills for isArray, isNaN and String.repeat

--- js/min/ie.lteIE9.js
+++ js/min/ie.lteIE9.js
@@ -17,6 +17,9 @@ if(!Array.prototype.forEach){Array.prototype.forEach=function(r,o){if(typeof r!=
 Date.now                = Date.now || function(){return +new Date};
 String.prototype.trim   = String.prototype.trim   || function(){return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");};
 Object.defineProperties = Object.defineProperties || function(obj, props){for(var i in props) Object.defineProperty(obj, i, props[i]);};
+Array.isArray           = Array.isArray           || function(obj){return "[object Array]" === Object.prototype.toString.call(obj)};
+Number.isNaN            = Number.isNaN            || function(val){return val !== val};
+String.prototype.repeat = String.prototype.repeat || function(num){return Array(num + 1).join(this)};
 
 
 /** Store "constants" on the window object to flag specific versions of Explorer. */
