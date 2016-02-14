66e5e6e8f3d3d7069a3c074969e2669d9895ecba
Alhadis <gardnerjohng@gmail.com>
2016-02-14 16:44:51 +1100

    Add function to fetch the deepest node inside an element

--- js/utils.js
+++ js/utils.js
@@ -713,6 +713,46 @@ function inside(node, selector, ignoreSelf){
 
 
 /**
+ * Return the deepest node within an element's descendants.
+ *
+ * @param {HTMLElement} el
+ * @return {Node}
+ */
+function deepest(el){
+	var children = el.querySelectorAll("*");
+	var branches = [];
+	var length   = children.length, i;
+	
+	/** Return the original element if there were no children */
+	if(!length) return el;
+	
+	for(i = 0; i < length; ++i){
+		var child  = children[i];
+		var depth  = 0;
+		var parent = child.parentNode;
+		
+		while(parent !== el){
+			++depth;
+			parent = parent.parentNode;
+		}
+		
+		branches.push([depth, child]);
+	}
+	
+	/** Ascertain which child had the greatest depth */
+	depth = [-1, null];
+	for(i = 0, length = branches.length; i < length; ++i){
+		child = branches[i];
+		if(child[0] > depth[0])
+			depth = child;
+	}
+	
+	return depth[1];
+}
+
+
+
+/**
  * Check if the user agent is a particular version of Internet Explorer.
  *
  * @param {String} version - The version to check against.
