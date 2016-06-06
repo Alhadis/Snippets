4c30a9125fc78d1de60654d3aa76efd0002892a2
Alhadis <gardnerjohng@gmail.com>
2016-06-07 00:59:28 +1000

    Add a reminder for how CoffeeScript loops are handled
    I can never remember what CoffeeScript's "of" operator does, since ES6's
    "of" operator keeps coming to mind first. The fact that CoffeeScript has
    an "in" operator that fills the role of the "of" operator in ES6 is less
    than ideal (to say the least).

---
+++ js/junk/coffeescript-loops.js
@@ -0,0 +1,26 @@
+
+for key, value of things
+
+	for (var key in things){
+		var value = things[k];
+	}
+
+for key of things
+	
+	for (key in things) {
+		
+	}
+
+
+for value, index in list
+
+	for (index = i = 0, len = list.length; i < len; index = ++i) {
+		value = list[index];
+	}
+
+
+for value in list
+	
+	for (i = 0, len = list.length; i < len; ++i) {
+		value = list[i];
+	}
