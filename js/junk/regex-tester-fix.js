419285987c74ef66ca90119c71e5c62c2efee275
Alhadis <gardnerjohng@gmail.com>
2016-09-08 20:41:55 +1000

    Tweak regex-tester fix
    * Input field now uses 11px Menlo, as per Atom setup
    * When run twice, unneeded elements disappear to provide more space
    
    I really should be testing regex in Atom, but I'm so used to this...

--- js/junk/regex-tester-fix.js
+++ js/junk/regex-tester-fix.js
@@ -7,11 +7,24 @@
 	"use strict";
 	var $ = function(s){ return document.querySelector(s) };
 
+	var pattern = $("#pattern");
+	pattern.style.font = "11px Menlo";
+
 	var input = $("#body");
-	input.style.tabSize    = 4;
-	input.style.fontSize   = "10pt";
-	input.style.fontFamily = "monospace";
-	input.spellcheck       = false;
+	if(input.patched){
+		var junk = document.querySelectorAll("#regexp-form > label, .options, img, footer, header, .tabs, #body, #replace");
+		Array.from(junk).forEach(function(a){a.hidden = true});
+		var css = document.styleSheets[0];
+		css.insertRule("[hidden]{display:none!important;}", css.rules.length);
+	}
+
+	else{
+		input.patched = true;
+		input.style.tabSize    = 4;
+		input.style.fontSize   = "10pt";
+		input.style.fontFamily = "monospace";
+		input.spellcheck       = false;
+	}
 
 	[].forEach.call($(".content").children, function(i){
 		i.spellcheck = false;
@@ -27,4 +40,4 @@
 
 
 /** Compressed variant for browser bookmarking: */
-javascript:(function(){"use strict";var e=function(e){return document.querySelector(e)};var t=e("#body");t.style.tabSize=4;t.style.fontSize="10pt";t.style.fontFamily="monospace";t.spellcheck=false;[].forEach.call(e(".content").children,function(e){e.spellcheck=false;var t=e.style;t.whiteSpace="pre";t.tabSize=4;t.fontFamily="monospace";t.fontSize="10pt";t.overflow="auto";t.width="100%"})})();
+javascript:!function(){"use strict";var e=function(e){return document.querySelector(e)},t=e("#pattern");t.style.font="11px Menlo";var o=e("#body");if(o.patched){var n=document.querySelectorAll("#regexp-form > label, .options, img, footer, header, .tabs, #body, #replace");Array.from(n).forEach(function(e){e.hidden=!0});var l=document.styleSheets[0];l.insertRule("[hidden]{display:none!important;}",l.rules.length)}else o.patched=!0,o.style.tabSize=4,o.style.fontSize="10pt",o.style.fontFamily="monospace",o.spellcheck=!1;[].forEach.call(e(".content").children,function(e){e.spellcheck=!1;var t=e.style;t.whiteSpace="pre",t.tabSize=4,t.fontFamily="monospace",t.fontSize="10pt",t.overflow="auto",t.width="100%"})}();
