095ed01fcdaa0d40b876350980389a2322e39cc2
Alhadis <gardnerjohng@gmail.com>
2016-10-01 17:39:49 +1000

    Add helper function to asynchronously load files

--- js/utils.js
+++ js/utils.js
@@ -1043,6 +1043,47 @@ function readStdin(){
 
 
 /**
+ * Asynchronously load a resource.
+ *
+ * Node-only.
+ *
+ * @param {String} url - URL or filesystem path
+ * @param {String} encoding - Defaults to "utf8"
+ * @return {Promise}
+ * @public
+ */
+function load(url, encoding = "utf8"){
+	return new Promise((resolve, reject) => {
+		const protocol = url.match(/^https?/);
+		
+		/** Remote resource: HTTPS or HTTP */
+		if(protocol){
+			let result = "";
+			const {get} = require(protocol[0].toLowerCase());
+			const request = get(url, response => {
+				if(response.statusMessage !== "OK")
+					return reject(response);
+				encoding && response.setEncoding(encoding);
+				response.on("data", s => result += s);
+				response.on("end", () => resolve(result));
+			});
+			request.on("error", e => reject(e));
+		}
+		
+		/** Assume parameter to be a filesystem path */
+		else{
+			const fs = require("fs");
+			fs.readFile(url, {encoding}, (error, data) => {
+				if(error) return reject(error);
+				return resolve(data.toString());
+			});
+		}
+	});
+}
+
+
+
+/**
  * Check if the user agent is a particular version of Internet Explorer.
  *
  * @param {String} version - The version to check against.
