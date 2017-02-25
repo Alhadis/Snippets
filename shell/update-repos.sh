79e8d28faabe706e9b3e81e418779d4bce560b75
Alhadis <gardnerjohng@gmail.com>
2017-02-26 05:42:35 +1100

    Refactor script for updating multiple repositories

--- shell/update-repos.sh
+++ shell/update-repos.sh
@@ -1,58 +1,89 @@
 #!/bin/sh
-
-# Switch the script's current directory
-cd `dirname $0`;
-
-# Neuter the internal field separator
-IFS=''
-
-# Formatting variables
-BOLD=`tput bold`
-BLUE=`tput setaf 12`
-RED=`tput setaf 9`
-RESET=`tput sgr0`
-
-# Include directories that start with a leading dot
 shopt -s dotglob
 
-
-# Print a divider to STDOUT to break up feedback better
-marker(){
-	printf %s$'\n' "${BOLD}$1==>${RESET}${BOLD} $2${RESET}";
+# Print current activity
+status(){
+	printf "\033[1;94m==>\033[0m\033[1m %s\033[0m"$'\n' "$1";
 }
 
-
-# Cycle through each item in the CWD
-for i in *; do
+# Update the repository of a VCS-controlled project folder
+update(){
+	i="$1"; [ -z "$i" ] && i=.;
 	
-	# Ensure the item's really a Git repository
-	[ -d "$i/.git" ] && { echo $(
+	# Git
+	[ -d "$i/.git" ] && {
+		status "Updating: $i"
+		cd $i && git status --porcelain && git pull;
 
-		cd $i;
-		
-		# This repo's been flagged as exempt from updates; skip it
-		[ $(git config updates.skip) ] && marker $RED "Skipping: $i" || {
-			marker $BLUE "Updating: $i"
-
-			# Update!
-			git status --porcelain && { git pull; };
+		# Prune remote-tracking branches
+		[ -z "$prune" ] || {
+			git fetch -p origin;
+			git remote show | grep -x fork >/dev/null && git fetch -p fork;
 		};
-
-	); };
-	
-	
-	# Is this a Subversion repository instead?
-	[ -d "$i/.svn" ] && { echo $(
 		
+		# Run housekeeping tasks
+		case "$gc" in
+			1) git gc && git prune -v;;
+			2) git gc --aggressive && git prune -v;;
+		esac
+		
+		cd ..;
+		return 0;
+	};
+	
+	# Subversion
+	[ -d "$i/.svn" ] && {
+		status "Updating: $i"
 		cd $i;
-		
-		# Drop another divider to break feedback up a little
-		marker $BLUE "Updating: $i"
-		
-		# Clear .DS_Store crap if possible
-		hash dsclean 2>/dev/null && dsclean;
-		
-		# Update!
+		hash dsclean 2>/dev/null && dsclean -q;
 		svn update;
-	); };
+		cd ..;
+		return 0;
+	};
+	
+	return 1;
+}
+
+# Display a short help notice and exit
+echo $* | grep -E '(\s|^)(--help|-[h?])(\\s|$)' >/dev/null && {
+	B=$(printf "\x1B[1m")
+	R=$(printf "\x1B[0m")
+	I=$(printf "\x1B[4m")
+	cat <<-EOF
+	${B}Usage:${R} update-repos [${B}-h${R}|${B}--help${R}] [${B}-gGp${R}] <${I}directories${R}>
+	
+	Update a folder of Git or SVN repositories.
+	
+	${B}Options:${R}
+	  ${B}-h${R}    Display this help output and exit
+	  ${B}-g${R}    Perform garbage collection and optimisation
+	  ${B}-G${R}    Perform more aggressive garbage collection
+	  ${B}-p${R}    Prune remote-tracking branches
+	
+	Run \`man update-repos\` for full documentation.
+	EOF
+	exit 0;
+};
+
+# Resolve options
+while getopts gGp option; do
+	case $option in
+		p)   prune=1 ;; # -p    Prune remote-tracking branches
+		g)   gc=1    ;; # -g    Perform garbage collection and optimisation
+		G)   gc=2    ;; # -G    Perform more aggressive garbage collection
+	esac
+done
+shift $((OPTIND - 1))
+
+# Resolve positional parameters
+[ $# -eq 0 ] && dirs=$(pwd) || dirs=($@)
+
+IFS=''
+
+# Look for VCS-enabled projects in each directory
+for i in ${dirs[@]}; do
+	cd "$i";
+	update "$i" || for i in *; do update "$i"; done;
 done;
+
+exit 0;
